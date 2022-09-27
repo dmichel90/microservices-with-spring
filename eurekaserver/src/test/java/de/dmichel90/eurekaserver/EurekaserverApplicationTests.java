7e2852ac992e0466271e2ee2fd7d5a45de70e7c1
Daniel Michel <danielmichel1190@gmail.com>
2022-09-27 23:48:48 +0200

    added eurekaserver

---
+++ eurekaserver/src/test/java/de/dmichel90/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dmichel90.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
