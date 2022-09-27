87419bf1e9850d9a626e23432489f5171b265654
Daniel Michel <danielmichel1190@gmail.com>
2022-09-27 23:46:08 +0200

    added configserver, gitignore and parent-pom

---
+++ configserver/src/main/java/de/dmichel90/configserver/SpringBootConfigserverApplication.java
@@ -0,0 +1,15 @@
+package de.dmichel90.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigserverApplication.class, args);
+	}
+
+}
---
+++ configserver/src/test/java/de/dmichel90/configserver/SpringBootConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dmichel90.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
