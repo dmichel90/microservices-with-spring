cf47c78ad0ac15339a48eb916c1a408189e44aab
Daniel Michel <danielmichel1190@gmail.com>
2022-10-04 16:09:38 +0200

    added small lib for common classes

--- lib/src/main/java/lib/KeycloakRoleConverter.java
+++ lib/src/main/java/lib/KeycloakRoleConverter.java
@@ -1,4 +1,4 @@
-package de.dmichel90.restaurant.config;
+package lib;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class KeycloakRoleConverter  implements Converter<Jwt, Collection<GrantedAuthority>> {
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
