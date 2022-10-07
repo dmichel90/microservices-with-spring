af1856ff029166830f23943fe67b8e2df31fa2c3
Daniel Michel <danielmichel1190@gmail.com>
2022-10-07 09:09:56 +0200

    rearranged openapi structure to use dto's for both microservices

--- docker/postgres/docker-entrypoint-initdb.d/docker_postgres_init.sql
+++ docker/postgres/docker-entrypoint-initdb.d/docker_postgres_init.sql
@@ -4,7 +4,7 @@ $do$
         IF
             NOT EXISTS(SELECT
                        FROM pg_roles
-                       WHERE rolname = 'restaurant_user') THEN
+                       WHERE rolname = 'restaurant_finder_user') THEN
             CREATE
                 USER "restaurant_finder_user" WITH PASSWORD 'secret' CREATEDB;
         END IF;
@@ -14,16 +14,16 @@ CREATE
     DATABASE "restaurant_db" WITH
     OWNER = "restaurant_finder_user"
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_EN.utf8'
-    LC_CTYPE = 'en_EN.utf8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 CREATE
     DATABASE "food_db" WITH
     OWNER = "restaurant_finder_user"
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_EN.utf8'
-    LC_CTYPE = 'en_EN.utf8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
